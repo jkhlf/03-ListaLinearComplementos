f746f2f3449123f67036b2b4e75d50411f072672
Joao Khalaf <joao.khalaf@gmail.com>
2024-03-19 10:40:37 -0300

    Exclusão de elemento.

--- ListaLinear/ListaLinear.cpp
+++ ListaLinear/ListaLinear.cpp
@@ -122,8 +122,20 @@ void inserirElemento()
 
 void excluirElemento()
 {
-
-
+	int valor;
+	cout << "Digite o elemento que quer exlcuir: " << endl;
+	cin >> valor;
+	int pos = posicaoElemento(valor);
+	if (pos != -1) {
+		for (int i = pos; i < nElementos - 1; i++) {
+			lista[i] = lista[i + 1];
+		} 
+		nElementos--;
+		cout << " O elemento excluido com sucesso." << endl;
+	}
+	else {
+		cout << "O elemento digitado não foi encontrado." << endl;
+	}
 }
 
 void buscarElemento()
@@ -148,6 +160,7 @@ int posicaoElemento(int busca)
 	for (int i = 0; i < nElementos; i++) {
 		if (busca == lista[i]) {
 			posicao = i;
+			break;
 		}
 	}
 	return posicao;
